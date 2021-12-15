6fb034460f75526cf1bc3d6ff43f52b9544ac72f
Vikum-AI <63595074+Vikum-AI@users.noreply.github.com>
2021-12-15 19:52:39 +0530

    Updated the less than function and greater than function

--- possion.py
+++ possion.py
@@ -2,29 +2,47 @@ import math
 
 data = []
 count = 0
-x = 4
-mean = 2.1
+# x = 4
+# mean = 2.1
+
 
 # Values to place in equation
-def input(): 
-    x  = int(input("Enter x value: "))
-    mean = float(input("Enter lambda value: "))
+x = int(input("Enter x value: "))
+mean = float(input("Enter lambda value: "))
 
 
-# P(X=x)
-def equals(): 
+# P(X = x)
+def equals():
     ans = (math.exp(-mean) * mean**x) / math.factorial(x)
-    print("P( X =" , x , ") = ", ans)
+    print("P( X =", x, ") = ", ans)
 
 
-# P(X<x)
+# P(X < x)
+def less_than():
+    for i in range(x):
+        # ans = (math.exp(-mean) * mean**i) / math.factorial(i)
+        ans = (math.exp(-mean) * mean**i) / math.factorial(i)
+        data.append(ans)
 
-for i in range(x): 
-    # ans = (math.exp(-mean) * mean**i) / math.factorial(i)
-    ans = (math.exp(-mean) * mean**i) / math.factorial(i)
-    print(i)
-    data.append(ans)
-    
+    for i in range(x):
+        total = data[i]
 
-print(data)
-    
+    print('')
+    print("P( X <", x, ") = ",  total)
+    print('')
+
+
+def greater_than():
+    for i in range(x):
+        # ans = (math.exp(-mean) * mean**i) / math.factorial(i)
+        ans = (math.exp(-mean) * mean**i) / math.factorial(i)
+        data.append(ans)
+
+    for i in range(x):
+        total = data[i]
+
+    total = 1 - total
+
+    print('')
+    print("P( X <", x, ") = ",  total)
+    print('')
